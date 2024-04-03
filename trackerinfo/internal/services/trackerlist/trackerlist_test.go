eedfd86572ae61ba4bd583cb919277757a88d0b5
Maksim <mribalko+git@gmail.com>
2024-04-03 15:01:05 +0400

    tests updated

--- trackerinfo/internal/services/trackerlist/trackerlist_test.go
+++ trackerinfo/internal/services/trackerlist/trackerlist_test.go
@@ -111,11 +111,27 @@ func TestTrackerList_RegisterSource(t *testing.T) {
 
 	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := tl.RegisterSource(tt.fetcher); (err != nil) == tt.expectError {
-				t.Error("expected error")
+			if err := tl.RegisterSource(tt.fetcher); (err != nil) != tt.expectError {
+				t.Error("expected error", err)
 			}
 		})
 	}
+
+	t.Run("can't register when running", func(t *testing.T) {
+		tl.StartUpdate(context.Background())
+
+		fetcher := testFetcher{name: "new", interval: 1}
+
+		err := tl.RegisterSource(&fetcher)
+		require.Error(t, err)
+
+		tl.StopUpdate()
+		time.Sleep(150 * time.Millisecond)
+		err = tl.RegisterSource(&fetcher)
+
+		require.NoError(t, err)
+
+	})
 
 }
 func TestTrackerList_Update(t *testing.T) {
@@ -144,14 +160,6 @@ func TestTrackerList_Update(t *testing.T) {
 		Longitude:   2,
 	}
 
-	testTracker2Up := models.Tracker{
-		OrigId:      "2",
-		Source:      "source2",
-		Description: "2_up",
-		Latitude:    2,
-		Longitude:   2,
-	}
-
 	fetcher1 := &testFetcher{
 		data:     []models.Tracker{testTracker1},
 		name:     "source1",
@@ -164,12 +172,10 @@ func TestTrackerList_Update(t *testing.T) {
 		interval: 10 * time.Second,
 	}
 
-	_ = testTracker1Up
-	_ = testTracker2Up
-
 	ctx := context.Background()
 
 	t.Run("Insertion to empty storage", func(t *testing.T) {
+		t.Parallel()
 		storage := &testStorage{}
 
 		tl, err := trackerlist.New(slogdiscard.NewDiscardLogger(), storage)
@@ -188,6 +194,7 @@ func TestTrackerList_Update(t *testing.T) {
 	})
 
 	t.Run("Insertion existing item", func(t *testing.T) {
+		t.Parallel()
 		storage := &testStorage{trackers: []models.Tracker{testTracker1}}
 
 		tl, err := trackerlist.New(slogdiscard.NewDiscardLogger(), storage)
@@ -205,6 +212,7 @@ func TestTrackerList_Update(t *testing.T) {
 	})
 
 	t.Run("Two fetchers empty storage", func(t *testing.T) {
+		t.Parallel()
 		storage := &testStorage{}
 
 		tl, err := trackerlist.New(slogdiscard.NewDiscardLogger(), storage)
@@ -219,9 +227,49 @@ func TestTrackerList_Update(t *testing.T) {
 		tl.StartUpdate(ctx)
 		time.Sleep(150 * time.Millisecond)
 		tl.StopUpdate()
-		assert.Equal(t, 2, storage.inserted, "no insertions expected")
+		assert.Equal(t, 2, storage.inserted, "2 insertions expected")
 		assert.Equal(t, 0, storage.deleted, "no deletions expected")
 		assert.Equal(t, 0, storage.updated, "no updates expected")
+	})
+
+	t.Run("Delete not existing", func(t *testing.T) {
+		t.Parallel()
+		storage := &testStorage{trackers: []models.Tracker{{
+			OrigId:      "2",
+			Source:      "source1",
+			Description: "2",
+			Latitude:    2,
+			Longitude:   2,
+		}}}
+
+		tl, err := trackerlist.New(slogdiscard.NewDiscardLogger(), storage)
+		require.NoError(t, err)
+
+		err = tl.RegisterSource(fetcher1)
+		require.NoError(t, err)
+
+		tl.StartUpdate(ctx)
+		time.Sleep(150 * time.Millisecond)
+		tl.StopUpdate()
+		assert.Equal(t, 1, storage.inserted, "1 insertion expected")
+		assert.Equal(t, 1, storage.deleted, "1 deletion expected")
+		assert.Equal(t, 0, storage.updated, "no updates expected")
+	})
+	t.Run("Update", func(t *testing.T) {
+		storage := &testStorage{trackers: []models.Tracker{testTracker1Up}}
+
+		tl, err := trackerlist.New(slogdiscard.NewDiscardLogger(), storage)
+		require.NoError(t, err)
+
+		err = tl.RegisterSource(fetcher1)
+		require.NoError(t, err)
+
+		tl.StartUpdate(ctx)
+		time.Sleep(150 * time.Millisecond)
+		tl.StopUpdate()
+		assert.Equal(t, 0, storage.inserted, "no insertions expected")
+		assert.Equal(t, 0, storage.deleted, "no deletions expected")
+		assert.Equal(t, 1, storage.updated, "1 update expected")
 	})
 
 }
